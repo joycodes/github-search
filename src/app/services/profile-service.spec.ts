06d4727a88b473e4aa2976de41761f8b764b22b9
Joyce Nguttu <jnguttu@gmail.com>
2022-04-19 07:02:36 +0300

    add blueprint user and class

--- src/app/services/profile-service.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ProfileService } from './profile-service';
-
-describe('ProfileService', () => {
-  it('should create an instance', () => {
-    expect(new ProfileService()).toBeTruthy();
-  });
-});
