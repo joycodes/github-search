15fff6129eafdc0efade865efe527bdefbcb7d26
Joyce Nguttu <jnguttu@gmail.com>
2022-04-19 05:09:49 +0300

    add input clearance on form submission

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
+
 export class ProfileComponent implements OnInit {
+  userProfile: any;
+  repos: any;
+  followers: any;
+  following: any;
+  username: string;
+  notFound = false;
 
-  constructor() { }
+  constructor(private service: ProfileService) { }
 
-  ngOnInit(): void {
+  searchUser() {
+    this.service.updateFields(this.username);
+    this.service.getProfileData()
+      .subscribe(profile => {
+        // console.log(profile);
+        this.userProfile = profile;
+      }, error => {
+        this.notFound = !this.notFound;
+      });
+    // clears input field
+    this.username = '';
+
+    this.service.getRepoData()
+      .subscribe(repos => {
+        this.repos = repos;
+        // console.log(repos);
+      });
+
+    this.service.getFollowers()
+      .subscribe(followers => {
+        this.followers = followers;
+      })
+
+    this.service.getFollowing()
+      .subscribe(following => {
+        this.following = following;
+      })
+  }
+
+  ngOnInit() {
   }
 
 }
+
