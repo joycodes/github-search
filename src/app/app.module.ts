1f127f1c5b73a5423a0207113f0fcea51c338047
Joyce Nguttu <jnguttu@gmail.com>
2022-04-19 05:02:57 +0300

    set up work environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { DateAgoPipe } from './date-ago.pipe';
+import { CardHoverDirective } from './card-hover.directive';
+import { FooterComponent } from './footer/footer.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    DateAgoPipe,
+    CardHoverDirective,
+    FooterComponent,
+    LandingPageComponent,
+    NavbarComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/directives/card-hover.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appCardHover]'
+})
+export class CardHoverDirective {
+
+  constructor() { }
+
+}
