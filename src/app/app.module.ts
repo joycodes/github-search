c9e9e71cbe0b558a9102ac660c4227b83fa1a7c5
Joyce Nguttu <jnguttu@gmail.com>
2022-04-19 06:39:08 +0300

    install progressbar packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,41 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
-import { DateAgoPipe } from './date-ago.pipe';
-import { CardHoverDirective } from './card-hover.directive';
-import { FooterComponent } from './footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ProfileService } from './services/profile.service';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { DateAgoPipe } from './pipes/date-ago.pipe';
+import { FooterComponent } from './footer/footer.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { CardHoverDirective } from './directives/card-hover.directive';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
-    DateAgoPipe,
-    CardHoverDirective,
-    FooterComponent,
     LandingPageComponent,
     NavbarComponent,
-    NotFoundComponent
+    DateAgoPipe,
+    FooterComponent,
+    NotFoundComponent,
+    CardHoverDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpClientModule
   ],
-  providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
