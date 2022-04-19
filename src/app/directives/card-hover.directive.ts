38dfe1a8261bd1b146ba9b8e1cb7255bb0807778
Joyce Nguttu <jnguttu@gmail.com>
2022-04-19 05:52:59 +0300

    add directive to repo cards, listens to hover event to change styling

--- src/app/directives/card-hover.directive.ts
+++ src/app/directives/card-hover.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appCardHover]'
 })
 export class CardHoverDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
+
+  @HostListener('mouseover') onOver() {
+    this.el.nativeElement.style.backgroundColor = 'rgb(248, 211, 0)';
+  }
+
+  @HostListener('mouseout') onOut() {
+    this.el.nativeElement.style.backgroundColor = '#fff';
+  }
 
 }
--- src/app/services/profile-service.ts
+++ src/app/services/profile-service.ts
@@ -1,2 +1,39 @@
+
+import { environment } from './../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
 export class ProfileService {
+  // to be moved to environment
+  private username: string;
+  private token = environment.token;
+  private clientId = environment.clientId;
+  private clientSecret = environment.clientSecret;
+
+  constructor(private http: HttpClient) {}
+
+  getProfileData() {
+    // return this.http.get(`https://api.github.com/users/${this.username}?access_token=${this.token}`);
+    return this.http.get(`https://api.github.com/users/${this.username}?${this.clientId}&client_secret=${this.clientSecret}`);
+  }
+
+  getRepoData() {
+    // return this.http.get(`https://api.github.com/users/${this.username}/repos?access_token=${this.token}`);
+    return this.http.get(`https://api.github.com/users/${this.username}/repos?${this.clientId}&client_secret=${this.clientSecret}`);
+  }
+
+  getFollowers() {
+    return this.http.get(`https://api.github.com/users/${this.username}/followers?${this.clientId}&client_secret=${this.clientSecret}`)
+  }
+
+  getFollowing() {
+    return this.http.get(`https://api.github.com/users/${this.username}/following?${this.clientId}&client_secret=${this.clientSecret}`)
+  }
+
+  updateFields(username: string) {
+    this.username = username;
+  }
 }
